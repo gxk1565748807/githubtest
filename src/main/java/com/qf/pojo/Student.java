d8b3e855d9de3f45cc2bc9bf37055ea24a937af3
gxk1565748807 <54383635+gxk1565748807@users.noreply.github.com>
2019-08-22 15:49:03 +0800

    Update Student.java

--- src/main/java/com/qf/pojo/Student.java
+++ src/main/java/com/qf/pojo/Student.java
@@ -2,6 +2,6 @@ package com.qf.pojo;
 
 public class Student {
     private Integer stuId;//学生id
-    private String stuName;//学生名称
-    private String stuPwd;//学生密码
+    private String stuName;//
+    private String stuPwd;//
 }
