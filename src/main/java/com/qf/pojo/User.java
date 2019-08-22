8f1f860eed5d8709037c469d0e8455dfff44b744
gxk1565748807 <1565748807@qq.com>
2019-08-22 15:19:55 +0800

    Initial commit

---
+++ src/main/java/com/qf/pojo/User.java
@@ -0,0 +1,10 @@
+package com.qf.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String userName;
+    private Integer userId;
+    private String userPwd;
+}
